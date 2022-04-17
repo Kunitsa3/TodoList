3eb6686181cf9b97e66a562b7caa1ecd3c7681b9
Olya Kunitsa <Kunitsa3@tut.by>
2022-04-17 20:18:33 +0300

    delete commented code

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,10 +1,5 @@
 import { format } from 'date-fns';
-
 import { TaskInformation } from 'types';
-
-// const id =
-//   ((JSON.parse(localStorage.getItem('tasksList')) as TaskInformation[])
-//     ?.reduce((max, curr) => (curr.id > max ? curr.id : max), 0) || 0) + 1;
 
 export const createTaskInformation = (task:string, id:string): TaskInformation =>
   ({ task, id, date: new Date(), isChecked: false });
